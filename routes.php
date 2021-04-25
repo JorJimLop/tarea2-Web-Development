221afd8930a00dc1f68656382fdf98ed3eab4e57
JorJimLop <jordan.jimenez.lopez@est.una.ac.cr>
2021-04-24 22:40:16 -0600

    hola

--- routes.php
+++ routes.php
@@ -3,7 +3,9 @@
     require_once('Author.php');
     require_once('Publisher.php'); 
    
-  Route::get('/', function () { return view('welcome'); });
+  //Route::get('/', function () { return view('welcome'); });
+    Route::get('/',function() { echo 'HOLA MUNDO!! Tarea 1, Jordan Jiménez López!'; });
+    
 
  
  Route::resource('author', 'AuthorController');
