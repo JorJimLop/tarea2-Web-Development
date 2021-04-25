1b3e0ec834d40864e234f064e6f0d452057f8096
JorJimLop <jordan.jimenez.lopez@est.una.ac.cr>
2021-04-25 00:09:17 -0600

    sdad

--- routes.php
+++ routes.php
@@ -1,15 +1,15 @@
 <?php
     require_once('Book.php');
-    require_once('Author.php');
+    require_once('author.php');
     require_once('Publisher.php'); 
     require_once('Professor.php'); 
     
    
   Route::get('/', function () { return view('welcome'); }); 
  
- Route::resource('/author', 'AuthorController');
-  Route::resource('/book', 'BookController');
-  Route::resource('/publisher', 'PublisherController');
-  Route::resource('/professor', 'ProfessorController');
+ Route::resource('author', 'AuthorController');
+  Route::resource('book', 'BookController');
+  Route::resource('publisher', 'PublisherController');
+  Route::resource('professor', 'ProfessorController');
     Route::dispatch();
 ?>
