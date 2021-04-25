9df359d860cba67dff135d52d5304625b05af8b9
JorJimLop <jordan.jimenez.lopez@est.una.ac.cr>
2021-04-24 23:44:02 -0600

    fatos

--- routes.php
+++ routes.php
@@ -2,6 +2,8 @@
     require_once('Book.php');
     require_once('Author.php');
     require_once('Publisher.php'); 
+    require_once('Professor.php'); 
+    
    
   Route::get('/', function () { return view('welcome'); });
 
@@ -9,5 +11,6 @@
  Route::resource('author', 'AuthorController');
   Route::resource('book', 'BookController');
   Route::resource('publisher', 'PublisherController');
+  Route::resource('professor', 'ProfessorController');
     Route::dispatch();
 ?>
