8507e29bbf3d3d8a37b7331f7535d7ffe4157735
JorJimLop <jordan.jimenez.lopez@est.una.ac.cr>
2021-04-24 22:24:53 -0600

    SAVES

--- Views/about.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo '<h1>This is a Page About!! </h1>'
-?>
- 
--- Views/welcome.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '<h1>This is a Page Main, Welcome!! </h1>'
- 
-?>
-<img src="./src/img/wel.jpg" style="width: 35%;display:block;
-margin:auto;"/>
