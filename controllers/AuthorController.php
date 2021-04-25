ac82218d8dd8fb195df94f22c916813e44687ddd
JorJimLop <jordan.jimenez.lopez@est.una.ac.cr>
2021-04-24 23:55:09 -0600

    cambios

--- controllers/AuthorController.php
+++ controllers/AuthorController.php
@@ -6,14 +6,14 @@
   class AuthorController extends Controller {
 
     public function index() {  
-      return view('authors/index',
+      return view('views/authors/index',
        ['author'=>Author::all(),
         'title'=>'authors List']);
     }
 
     public function show($id) {
       $aut = Author::find($id);
-      return view('authors/show',
+      return view('views/authors/show',
         ['author'=>$aut,
          'title'=>'authors Detail']);
     }
